cde93bfe99c9a1630a6cc326fb7d5754a8e983c5
David Liu <mzliu@transunion.com>
2015-02-17 00:39:22 -0600

    Create student entity.

---
+++ src/main/java/com/tu/myapp/entity/Student.java
@@ -0,0 +1,40 @@
+package com.tu.myapp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="STUDENT")
+public class Student {
+	   private Integer age;
+	   private String name;
+	   @Id
+	   @Column(name="id")
+	   @GeneratedValue(strategy=GenerationType.IDENTITY)
+	   private Integer id;
+
+	   public void setAge(Integer age) {
+	      this.age = age;
+	   }
+	   public Integer getAge() {
+	      return age;
+	   }
+
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+	   public String getName() {
+	      return name;
+	   }
+
+	   public void setId(Integer id) {
+	      this.id = id;
+	   }
+	   public Integer getId() {
+	      return id;
+	   }
+}
